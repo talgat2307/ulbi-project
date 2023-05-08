7f80785b2acb5e616d78d92aea2c996fb5036a0b
Talgat Muratbekov <talgat2307@gmail.com>
2023-05-08 20:27:11 +0600

    fix Sidebar folder naming

---
+++ src/widgets/Sidebar1/ui/Sidebar.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './Sidebar.module.scss';
+
+interface SidebarProps {
+  className?: string
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  const { t } = useTranslation();
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleToggle = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <div
+      data-testid="sidebar"
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+    >
+      <Button
+        data-testid="sidebar-toggle"
+        type="button"
+        onClick={handleToggle}
+      >
+        {t('toggle')}
+      </Button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher />
+      </div>
+    </div>
+  );
+};
